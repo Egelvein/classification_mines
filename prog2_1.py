09c05c53040028ca43638edd794ca8be56718f83
Egelvein <81914467+Egelvein@users.noreply.github.com>
2023-02-26 21:12:34 +0300

    Update prog2_1.py

--- prog2_1.py
+++ prog2_1.py
@@ -12,13 +12,7 @@ train_datagen = ImageDataGenerator(
     height_shift_range=0.2,
     shear_range=0.2,
     horizontal_flip=True)
-test_datagen = ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=40,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    horizontal_flip=True)
+test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
     '/home/vyacheslav/Учёба/Диплом/12.11.2022/mines_jpg/train',
@@ -44,6 +38,7 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(128, (3, 3), activation = 'relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(512, activation = 'relu'))
 model.add(layers.Dense(1, activation = 'sigmoid'))
 
